dffacb47261182add591b713bdef42e4285c1d3e
nickware44 <support@nickware.group>
2025-11-27 12:07:33 +0300

    `OpenCL` backend: reset function implementation

--- src/translators/cl.cpp
+++ src/translators/cl.cpp
@@ -14,6 +14,7 @@
 void* indk::Translators::CL::doTranslate(const std::vector<indk::Neuron*>& neurons, const std::vector<std::string> &outputs, const indk::StateSyncMap& sync) {
     auto model = new ModelData;
 
+#ifdef INDK_OPENCL_SUPPORT
     model -> objects = neurons;
     model -> outputs = outputs;
     model -> pair_pool_size = 0;
@@ -27,7 +28,6 @@ void* indk::Translators::CL::doTranslate(const std::vector<indk::Neuron*>& neuro
         model -> input_pool_size += n->getEntriesCount();
     }
 
-#ifdef INDK_OPENCL_SUPPORT
     model -> PairsInfo = new cl_float16[model->pair_pool_size];
     model -> ReceptorsInfo = new cl_float8[model->receptor_pool_size];
     model -> NeuronsInfo = new cl_float3[model->neuron_pool_size];
@@ -36,13 +36,23 @@ void* indk::Translators::CL::doTranslate(const std::vector<indk::Neuron*>& neuro
     model -> Times = new cl_int[model->neuron_pool_size];
     memset(model->Outputs, 0, sizeof(cl_float)*model->neuron_pool_size);
 
+    doReset(model);
+#endif
+
+    return model;
+}
+
+void indk::Translators::CL::doReset(indk::Translators::CL::ModelData *model) {
+    memset(model->Outputs, 0, sizeof(cl_float)*model->neuron_pool_size);
+
+#ifdef INDK_OPENCL_SUPPORT
     indk::Position *rpos, *spos;
     uint64_t px = 0, pxstart;
     uint64_t rx = 0, rxstart;
     uint64_t ex = 0, exstart;
 
-    for (uint64_t ni = 0; ni < neurons.size(); ni++) {
-        auto n = (indk::Neuron*)neurons[ni];
+    for (uint64_t ni = 0; ni < model->objects.size(); ni++) {
+        auto n = model->objects[ni];
         auto elist = n -> getEntries();
 
         rxstart = rx;
@@ -63,66 +73,38 @@ void* indk::Translators::CL::doTranslate(const std::vector<indk::Neuron*>& neuro
                     spos = s -> getPos();
 
                     model -> PairsInfo[px] = {
-                        static_cast<cl_float>(rpos->getPositionValue(0)),
-                        static_cast<cl_float>(rpos->getPositionValue(1)),
-                        static_cast<cl_float>(spos->getPositionValue(0)),
-                        static_cast<cl_float>(spos->getPositionValue(1)),
-                        static_cast<cl_float>(s->getGamma()),
-                        static_cast<cl_float>(ex),
+                            static_cast<cl_float>(rpos->getPositionValue(0)),
+                            static_cast<cl_float>(rpos->getPositionValue(1)),
+                            static_cast<cl_float>(spos->getPositionValue(0)),
+                            static_cast<cl_float>(spos->getPositionValue(1)),
+                            static_cast<cl_float>(s->getGamma()),
+                            static_cast<cl_float>(ex),
 
-                        static_cast<cl_float>(s->getLambda()),
-                        static_cast<cl_float>(s->getk1()),
-                        static_cast<cl_float>(s->getk2()),
+                            static_cast<cl_float>(s->getLambda()),
+                            static_cast<cl_float>(s->getk1()),
+                            static_cast<cl_float>(s->getk2()),
                     };
                     px++;
                 }
                 ex++;
             }
             model -> ReceptorsInfo[rx] = {
-                static_cast<cl_float>(pxstart),
-                static_cast<cl_float>(px),
-                static_cast<cl_float>(exstart),
-                static_cast<cl_float>(r->getRs()),
-                static_cast<cl_float>(r->getFi()),
-                static_cast<cl_float>(r->getk3()),
+                    static_cast<cl_float>(pxstart),
+                    static_cast<cl_float>(px),
+                    static_cast<cl_float>(exstart),
+                    static_cast<cl_float>(r->getRs()),
+                    static_cast<cl_float>(r->getFi()),
+                    static_cast<cl_float>(r->getk3()),
             };
             rx++;
         }
         model -> NeuronsInfo[ni] = {
-            static_cast<cl_float>(rxstart),
-            static_cast<cl_float>(rx),
-            static_cast<cl_float>(exstart)
+                static_cast<cl_float>(rxstart),
+                static_cast<cl_float>(rx),
+                static_cast<cl_float>(exstart)
         };
     }
 #endif
-
-    return model;
-}
-
-void indk::Translators::CL::doReset(indk::Translators::CL::ModelData *model) {
-    memset(model->Outputs, 0, sizeof(cl_float)*model->neuron_pool_size);
-
-//    for (const auto &o: model->objects) {
-//        auto neuron = o.second;
-//        neuron -> t = 0;
-//
-//        for (int i = 0; i < neuron->receptor_count; i++) {
-//            auto r = o.second -> receptors[i];
-//            r.fi = 0;
-//            r.l = 0;
-//            r.rs = r.rs_default;
-//            std::memcpy(r.position, r.position_default, sizeof(float)*o.second->dimension_count);
-//        }
-//        for (int j = 0; j < neuron->entry_count; j++) {
-//            auto e = o.second->entries[j];
-//            for (unsigned int k = 0; k < e.synapse_count; k++) {
-//                e.synapses[k].gamma = 0;
-//                e.synapses[k].d_gamma = 0;
-//                e.synapses[k].l_gamma = 0;
-//                e.synapses[k].l_d_gamma = 0;
-//            }
-//        }
-//    }
 }
 
 std::string indk::Translators::CL::getTranslatorName() {
